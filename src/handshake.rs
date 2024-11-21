4df975483ea7042ff5fca917adc406ffc17db1eb
roson <roson9527@sina.cn>
2024-11-21 17:41:45 +0800

    feat(handshake): update StreamHandshake trait for improved handling

--- src/handshake.rs
+++ src/handshake.rs
@@ -1,17 +1,33 @@
 use crate::types::{PSTReceiver, PSTSender};
 use async_trait::async_trait;
 use eyre::Result as EResult;
+use futures_util::SinkExt;
+use tokio_stream::StreamExt;
+use tungstenite::Message;
 
 #[async_trait]
 pub trait StreamHandshake {
-    async fn handshake(&self, writer: &PSTSender, reader: &PSTReceiver) -> EResult<()>;
+    async fn handshake(&self, writer: &mut PSTSender, reader: &mut PSTReceiver) -> EResult<()>;
 }
 
 pub struct NonHandshake;
 
 #[async_trait]
 impl StreamHandshake for NonHandshake {
-    async fn handshake(&self, _writer: &PSTSender, _reader: &PSTReceiver) -> EResult<()> {
+    async fn handshake(&self, _writer: &mut PSTSender, _reader: &mut PSTReceiver) -> EResult<()> {
+        Ok(())
+    }
+}
+
+pub struct SingleHandshake;
+
+#[async_trait]
+impl StreamHandshake for SingleHandshake {
+    async fn handshake(&self, writer: &mut PSTSender, reader: &mut PSTReceiver) -> EResult<()> {
+        let _ = writer
+            .send(Message::Text("hello world".to_string()))
+            .await?;
+        let _ = reader.next().await;
         Ok(())
     }
 }
